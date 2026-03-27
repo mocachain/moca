7c806b64dd7c42cc4c03b7387cd158999081b0cc
Luke <luchenqun@qq.com>
2026-03-27 10:46:45 +0800

    fix: use gRPC for permission CLI params query
    Replace EVM precompile Params() with types.NewQueryClient(clientCtx).Params
    so CLI unit tests work with mocked CometBFT RPC only (no ethclient).
    
    Align with greenfield x/permission/client/cli/query_params.go.
    Same approach as PR #132 (sp) and #135 (storage).

--- x/permission/client/cli/query_params.go
+++ x/permission/client/cli/query_params.go
@@ -1,14 +1,12 @@
 package cli
 
 import (
+	"context"
+
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/spf13/cobra"
 
-	evmostypes "github.com/evmos/evmos/v12/types"
-	"github.com/evmos/evmos/v12/x/evm/precompiles/permission"
 	"github.com/evmos/evmos/v12/x/permission/types"
 )
 
@@ -19,20 +17,13 @@ func CmdQueryParams() *cobra.Command {
 		Args:  cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, _ []string) error {
 			clientCtx := client.GetClientContextFromCmd(cmd)
-			contract, err := permission.NewIPermission(common.HexToAddress(evmostypes.PermissionAddress), clientCtx.EvmClient)
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.Params(context.Background(), &types.QueryParamsRequest{})
 			if err != nil {
 				return err
 			}
-			result, err := contract.Params(&bind.CallOpts{})
-			if err != nil {
-				return err
-			}
-			res := &types.QueryParamsResponse{
-				Params: types.Params{
-					MaximumStatementsNum:                  result.MaximumStatementsNum,
-					MaximumGroupNum:                       result.MaximumGroupNum,
-					MaximumRemoveExpiredPoliciesIteration: result.MaximumRemoveExpiredPoliciesIteration,
-				}}
 
 			return clientCtx.PrintProto(res)
 		},
