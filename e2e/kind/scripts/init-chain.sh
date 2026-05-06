480906e0366fc419a1e0a6c9b5aa66daae72eb87
Luke <luchenqun@qq.com>
2026-05-07 02:15:46 +0800

    fix(deployment): configure timeout commit and snapshot interval (#203)
    Add TIMEOUT_COMMIT to deployment and e2e env templates so scripts do not keep
    timeout_commit fixed when generating config.toml.
    
    Update snapshot-interval replacements to match any numeric value, not only
    0, so configured values are applied consistently.

--- e2e/kind/scripts/init-chain.sh
+++ e2e/kind/scripts/init-chain.sh
@@ -336,7 +336,7 @@ generate_genesis() {
     sed -i "s/timeout_prevote_delta = \"500ms\"/timeout_prevote_delta = \"200ms\"/g" "$config"
     sed -i "s/timeout_precommit = \"1s\"/timeout_precommit = \"500ms\"/g" "$config"
     sed -i "s/timeout_precommit_delta = \"500ms\"/timeout_precommit_delta = \"200ms\"/g" "$config"
-    sed -i "s/timeout_commit = \"3s\"/timeout_commit = \"500ms\"/g" "$config"
+    sed -i "s/timeout_commit = \"3s\"/timeout_commit = \"${TIMEOUT_COMMIT:-1s}\"/g" "$config"
     sed -i "s/addr_book_strict = true/addr_book_strict = false/g" "$config"
     sed -i "s/allow_duplicate_ip = false/allow_duplicate_ip = true/g" "$config"
     sed -i "s/log_level = \"info\"/log_level = \"debug\"/g" "$config"
@@ -344,7 +344,7 @@ generate_genesis() {
 
     # ---- app.toml patches ----
     sed -i "s/minimum-gas-prices = \"0amoca\"/minimum-gas-prices = \"5000000000${BASIC_DENOM}\"/g" "$app"
-    sed -i "s/snapshot-interval = 0/snapshot-interval = ${SNAPSHOT_INTERVAL}/g" "$app"
+    sed -i "s/snapshot-interval = [0-9][0-9]*/snapshot-interval = ${SNAPSHOT_INTERVAL}/g" "$app"
     sed -i "s/snapshot-keep-recent = 2/snapshot-keep-recent = ${SNAPSHOT_KEEP_RECENT}/g" "$app"
     sed -i "s/src-chain-id = 1/src-chain-id = ${SRC_CHAIN_ID}/g" "$app"
     sed -i "s/dest-bsc-chain-id = 2/dest-bsc-chain-id = ${DEST_CHAIN_ID}/g" "$app"
