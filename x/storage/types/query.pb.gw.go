954254fab70a04bb4c120f5e51f93765ccf809f6
puneetmahajan <59960662+puneet2019@users.noreply.github.com>
2026-05-08 16:06:39 +0400

    chore: regenerate .pb.go after option go_package rename
    `make proto-gen` regenerates the FileDescriptorProto bytes embedded in
    each .pb.go to reflect the new option go_package = "github.com/mocachain/moca/v2/..."
    paths. The earlier sed-only pass updated proto file declarations and
    import paths in .go but not the compressed proto descriptors.
    
    72 .pb.go files updated. No source-level changes; mechanical regen.

--- x/storage/types/query.pb.gw.go
+++ x/storage/types/query.pb.gw.go
@@ -13,11 +13,11 @@ import (
 	"io"
 	"net/http"
 
-	"github.com/mocachain/moca/v2/x/permission/types"
 	"github.com/golang/protobuf/descriptor"
 	"github.com/golang/protobuf/proto"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/grpc-ecosystem/grpc-gateway/utilities"
+	"github.com/mocachain/moca/v2/x/permission/types"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/grpclog"
